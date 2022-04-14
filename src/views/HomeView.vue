e8da20d45f18b69b1212ff6ccd8d26071409f895
dike <dike@kuaishou.com>
2022-04-14 15:27:06 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="home">
+    <h1>This is an home page</h1>
+  </div>
+</template>
+
+<style>
+.home {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ vetur.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+// vetur.config.js
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+  settings: {
+    "vetur.useWorkspaceDependencies": true,
+    "vetur.experimental.templateInterpolationService": true,
+  },
+};
