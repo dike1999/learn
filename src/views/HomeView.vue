0107839d95bc24bd67814e08964690dd65fdb0df
dike <dike@kuaishou.com>
2022-04-14 15:56:56 +0800

    init2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,33 @@
-<script setup lang="ts"></script>
-
 <template>
   <div class="home">
-    <h1>This is an home page</h1>
+    <h1>home page</h1>
+    <div class="count" @click="handleSubmit">
+      Click
+      <span>{{ count }}</span>
+    </div>
   </div>
 </template>
 
-<style>
+<script setup lang="ts">
+import { ref } from "vue";
+const count = ref(0);
+
+const handleSubmit = () => {
+  count.value++;
+};
+</script>
+
+<style scoped lang="less">
 .home {
   display: flex;
-  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.count {
+  font-size: 20px;
+
+  span {
+    color: blue;
+  }
 }
 </style>
