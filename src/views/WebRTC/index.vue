59729d0034bf1b72a111614acd47c4db617d2598
dike <dike@kuaishou.com>
2022-04-18 15:25:02 +0800

    feat: webrtc getUserMedia

--- src/views/WebRTC/index.vue
+++ src/views/WebRTC/index.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="webRTC">
     <h1>This is an WebRTC page</h1>
+    <video autoplay id="player" ref="player" />
+    <div>{{ errMsg }}</div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const player = ref<HTMLMediaElement | null>(null);
+const errMsg = ref("");
+
+onMounted(() => {
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    errMsg.value = "getUserMedia is not supported";
+  } else {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        if (player.value) player.value.srcObject = stream;
+      })
+      .catch((err) => {
+        errMsg.value = err;
+      });
+  }
+});
+</script>
 <style scoped></style>
