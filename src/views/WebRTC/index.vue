28577183fc73852d9c8001f2e0ac1e72009090cc
dike <dike@kuaishou.com>
2022-04-18 15:57:52 +0800

    feat: add media options

--- src/views/WebRTC/index.vue
+++ src/views/WebRTC/index.vue
@@ -2,14 +2,54 @@
   <div class="webRTC">
     <h1>This is an WebRTC page</h1>
     <video autoplay id="player" ref="player" />
-    <div>{{ errMsg }}</div>
+    <div style="display: flex">
+      <div>
+        <label>audio Source:</label>
+        <select id="audioSource">
+          <option
+            v-for="item in audioSource"
+            :value="item.deviceId"
+            :text="item.label"
+            :key="item.deviceId"
+          />
+        </select>
+      </div>
+
+      <div>
+        <label>audio Output:</label>
+        <select id="audioOutput">
+          <option
+            v-for="item in audioOutput"
+            :value="item.deviceId"
+            :text="item.label"
+            :key="item.deviceId"
+          />
+        </select>
+      </div>
+
+      <div>
+        <label>video Source:</label>
+        <select id="videoSource">
+          <option
+            v-for="item in videoSource"
+            :value="item.deviceId"
+            :text="item.label"
+            :key="item.deviceId"
+          />
+        </select>
+      </div>
+    </div>
   </div>
+  <div>{{ errMsg }}</div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, ref, reactive } from "vue";
 
 const player = ref<HTMLMediaElement | null>(null);
+const audioSource = reactive<MediaDeviceInfo[]>([]);
+const audioOutput = reactive<MediaDeviceInfo[]>([]);
+const videoSource = reactive<MediaDeviceInfo[]>([]);
 const errMsg = ref("");
 
 onMounted(() => {
@@ -20,6 +60,18 @@ onMounted(() => {
       .getUserMedia({ video: true, audio: true })
       .then((stream) => {
         if (player.value) player.value.srcObject = stream;
+        return navigator.mediaDevices.enumerateDevices();
+      })
+      .then((deviceinfo) => {
+        deviceinfo.forEach((device) => {
+          if (device.kind === "audioinput") {
+            audioSource.push(device);
+          } else if (device.kind === "audiooutput") {
+            audioOutput.push(device);
+          } else if (device.kind === "videoinput") {
+            videoSource.push(device);
+          }
+        });
       })
       .catch((err) => {
         errMsg.value = err;
