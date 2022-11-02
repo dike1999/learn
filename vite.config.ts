f5a3defa61f083c3e3953b6458891eba9c06af7a
dike <dike@kuaishou.com>
2022-11-02 16:13:22 +0800

    feat: 关闭__VUE_OPTIONS_API__

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __VUE_OPTIONS_API__: false,
+  },
   plugins: [
     vue(),
     vueJsx(),
