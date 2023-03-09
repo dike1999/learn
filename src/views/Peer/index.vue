15acccf751e1fcd114112f65f880fd24e6e4f51c
dike <1138832931@qq.com>
2023-03-09 23:41:38 +0800

    feat: video

--- src/views/Peer/index.vue
+++ src/views/Peer/index.vue
@@ -4,6 +4,44 @@
       <div class="user-info-id">{{ userId }}</div>
       <el-button type="primary" @click="onCopy">复制</el-button>
     </div>
+    <div class="videos">
+      <div class="video">
+        <h3 class="title">Local</h3>
+        <video
+          autoplay
+          height="300"
+          width="300"
+          id="localVideo"
+          ref="localVideo"
+        />
+        <h3 class="title">OfferSDP</h3>
+        <!-- <el-input
+          v-model="offerSDP"
+          :autosize="{ minRows: 2, maxRows: 8 }"
+          type="textarea"
+          placeholder="OfferSDP"
+          disabled
+        /> -->
+      </div>
+      <div class="video">
+        <h3 class="title">Remote</h3>
+        <video
+          autoplay
+          height="300"
+          width="300"
+          id="remoteVideo"
+          ref="remoteVideo"
+        />
+        <h3 class="title">AnswerSDP</h3>
+        <!-- <el-input
+          v-model="answerSDP"
+          :autosize="{ minRows: 2, maxRows: 8 }"
+          type="textarea"
+          placeholder="AnswerSDP"
+          disabled
+        /> -->
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,7 +52,10 @@ import clipboard from "clipboard";
 import { ElMessage } from "element-plus";
 import "element-plus/es/components/message/style/css";
 
-const userId = ref<string>("");
+const userId = ref<string>(""); // 个人信息
+const localVideo = ref<HTMLMediaElement | null>(null); // 本地
+const remoteVideo = ref<HTMLMediaElement | null>(null); // 远程
+
 const peer = ref<Peer>(
   new Peer({
     host: "101.201.140.172",
@@ -41,13 +82,8 @@ onUnmounted(() => {
 </script>
 
 <style scoped lang="less">
-.peer {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
 .user-info {
+  background: #666;
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -56,6 +92,19 @@ onUnmounted(() => {
 
   .user-info-id {
     font-size: 24px;
+  }
+}
+
+.videos {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  .video {
+    flex: auto;
+    .title {
+      background-color: #f0c0b8;
+      text-align: center;
+    }
   }
 }
 </style>
