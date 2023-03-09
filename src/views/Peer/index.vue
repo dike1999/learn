6eb88f05e374ab480460fdaa4e734867216c2c95
dike <1138832931@qq.com>
2023-03-09 23:29:12 +0800

    feat: use peer

--- src/views/Peer/index.vue
+++ src/views/Peer/index.vue
@@ -1,20 +1,43 @@
 <template>
   <div class="peer">
-    <h1>peer page</h1>
+    <div class="user-info">
+      <div class="user-info-id">{{ userId }}</div>
+      <el-button type="primary" @click="onCopy">复制</el-button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, reactive } from "vue";
+import { ref, onUnmounted } from "vue";
 import Peer from "peerjs";
+import clipboard from "clipboard";
+import { ElMessage } from "element-plus";
+import "element-plus/es/components/message/style/css";
 
+const userId = ref<string>("");
 const peer = ref<Peer>(
   new Peer({
     host: "101.201.140.172",
     path: "peer",
-    port: "9011",
+    port: 9011,
   })
 );
+peer.value
+  .on("open", (id) => {
+    userId.value = id;
+  })
+  .on("disconnected", () => {
+    console.log("disconnected");
+  });
+
+const onCopy = () => {
+  clipboard.copy(userId.value);
+  ElMessage.success("复制成功");
+};
+
+onUnmounted(() => {
+  peer.value.destroy();
+});
 </script>
 
 <style scoped lang="less">
@@ -22,5 +45,17 @@ const peer = ref<Peer>(
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.user-info {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+
+  .user-info-id {
+    font-size: 24px;
+  }
 }
 </style>
